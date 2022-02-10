c761760d65aa341b3ccf0c867886d7ee3830976e
Christian <86079954+ChristianCloud9@users.noreply.github.com>
2022-02-10 22:02:59 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;  // pi value can never be changed 
+            
+
+            Console.Write("Enter radius: ");  // User enters a radius 
+            double radious = Convert.ToDouble(Console.ReadLine());  // Converts users input to a double 
+            double Area = pi * radious * radious;   // Formula for Area of a circle 
+            double result = Math.Round(Area, 1, MidpointRounding.AwayFromZero);   // Answer is then rounded to the first decimal point 
+
+            Console.WriteLine("Area of a circle: " + result); // displays the answer to area of the circle 
+            Console.ReadKey();
+        }
+    }
+}
